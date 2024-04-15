05d68b88081d4d0856cd355d812a90c41d820dce
GilbertChenHoltville <holtvillechen@gmail.com>
2024-04-15 16:44:03 +0800

    center with arrows

--- GBA_pose_estimation.py
+++ GBA_pose_estimation.py
@@ -32,19 +32,19 @@ def Regression4pts(_4pts, depth_img, json_file):
     if depth_img[LT_asn[1]][LT_asn[0]] != 0:
         b_LT_ori_pix_exist = True
         LT_world_coord = GBA_construct_cloud.pixel_to_world(json_file, LT_asn[0], LT_asn[1], depth_img[LT_asn[1]][LT_asn[0]])
-        # print("LT", LT_world_coord)
+        print("LT", LT_world_coord)
     if depth_img[RT_asn[1]][RT_asn[0]] != 0:
         b_RT_ori_pix_exist = True
         RT_world_coord = GBA_construct_cloud.pixel_to_world(json_file, RT_asn[0], RT_asn[1], depth_img[RT_asn[1]][RT_asn[0]])
-        # print("RT", RT_world_coord)
+        print("RT", RT_world_coord)
     if depth_img[RB_asn[1]][RB_asn[0]] != 0:
         b_RB_ori_pix_exist = True
         RB_world_coord = GBA_construct_cloud.pixel_to_world(json_file, RB_asn[0], RB_asn[1], depth_img[RB_asn[1]][RB_asn[0]])
-        # print("RB", RB_world_coord)
+        print("RB", RB_world_coord)
     if depth_img[LB_asn[1]][LB_asn[0]] != 0:
         b_LB_ori_pix_exist = True
         LB_world_coord = GBA_construct_cloud.pixel_to_world(json_file, LB_asn[0], LB_asn[1], depth_img[LB_asn[1]][LB_asn[0]])
-        # print("LB", LB_world_coord)
+        print("LB", LB_world_coord)
     if b_LT_ori_pix_exist and b_RT_ori_pix_exist:
         right_arrow = RT_world_coord - LT_world_coord
     elif b_LB_ori_pix_exist and b_RB_ori_pix_exist:
@@ -53,12 +53,20 @@ def Regression4pts(_4pts, depth_img, json_file):
         print("regression version 1 failed")
         return None
     if b_LT_ori_pix_exist and b_LB_ori_pix_exist:
-        up_arrow = LT_world_coord - LB_world_coord
+        down_arrow = LB_world_coord - LT_world_coord
     elif b_RT_ori_pix_exist and b_RB_ori_pix_exist:
-        up_arrow = RT_world_coord - RB_world_coord
+        down_arrow = RB_world_coord - RT_world_coord
+    else:
+        print("regression version 1 failed")
+        return None
+    if b_LT_ori_pix_exist:
+        center = LT_world_coord + right_arrow/2. + down_arrow/2.
+    elif b_RT_ori_pix_exist:
+        center = RT_world_coord - right_arrow/2. + down_arrow/2.
     else:
         print("regression version 1 failed")
         return None
     # print("right_arrow", right_arrow)
-    # print("up arrow", up_arrow)
-    return [right_arrow, up_arrow]
+    # print("down arrow", down_arrow)
+    # print("center", center)
+    return [right_arrow, down_arrow, center]
